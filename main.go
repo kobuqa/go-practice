e781810fcaa8fd065a0199ac998b94a6719c3a98
Матвейчиков Александр <matveychikov.a@wb.ru>
2024-12-14 22:53:37 +0300

    feat: .env and http server

--- main.go
+++ main.go
@@ -1,5 +1,27 @@
 package main
 
+import (
+	"fmt"
+	"log"
+	"net/http"
+
+	"github.com/kobuqa/hello-world-service/config"
+)
+
 func main() {
-	println("Hello World")
+	cfg, err := config.LoadConfig()
+	if err != nil {
+		log.Fatal("Failed to load configuration:", err)
+	}
+
+	http.HandleFunc("/", func(w http.ResponseWriter, r *http.Request) {
+		w.Write([]byte("Hello, World!"))
+	})
+
+	serverAddr := fmt.Sprintf("%s:%s", cfg.ServerHost, cfg.ServerPort)
+	log.Printf("Server starting on %s in %s mode...", serverAddr, cfg.Env)
+
+	if err := http.ListenAndServe(serverAddr, nil); err != nil {
+		log.Fatal(err)
+	}
 }
